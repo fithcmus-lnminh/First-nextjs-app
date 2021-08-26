03e4959cbae8ba27352f23ddbe70bc122d43d3dd
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-26 22:52:09 +0700

    get the dynamic parameter value

--- pages/news/[newId].js
+++ pages/news/[newId].js
@@ -1,7 +1,15 @@
 // domain.com/news/detail
 //use [...].js for dymamic routing in nextjs
+import { useRouter } from "next/router";
 
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newId = router.query.newId; //get newId (after news/)
+
+  //send request
+  //fetch news with newId
+
   return <h1>The News Detail Page</h1>;
 };
 
