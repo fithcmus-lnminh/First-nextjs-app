5fc50702641eeffd73b818fec1def6a6fc4d8542
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-26 23:15:03 +0700

    use Link from next/link to link between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
 // domain.com/news
+import Link from "next/link";
 
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <div>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>
+          <Link href="news/abc">Go to abc</Link>
+        </li>
+        <li>
+          <Link href="news/xyz">Go to xyz</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default NewsPage;
